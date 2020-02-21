33a7d784c011c81989fab402e7dd3ff8275b1a24
Alexei Kravchuk <61118070+alexeikravchuk@users.noreply.github.com>
2020-02-21 23:07:57 +0300

    feat: task done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (array = []) {
+    if(array.length === 0) return 0;
+    let minValue = array[0];
+    for(let i of array) {
+        if(i < minValue) {
+            minValue = i;
+        }
+    }
+    return minValue;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (array = []) {
+    if(array.length === 0) return 0;
+    let maxValue = array[0];
+    for(let i of array) {
+        if(i > maxValue) {
+                maxValue = i;
+        }
+    }
+    return maxValue;
 }
-
-exports.avg = function avg (array) {
-  return 0;
+  
+exports.avg = function avg (array = []) {
+    if(array.length === 0) return 0;
+    let sum = 0;
+    for(let i of array) {
+        sum += i;
+    }
+    return sum / array.length;
 }
